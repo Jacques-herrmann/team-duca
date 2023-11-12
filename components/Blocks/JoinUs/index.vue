7e4b5939230f879594d27dd384e69655361c2092
Jacques <jacquesherrmann@gmail.com>
2023-11-12 14:34:52 +0100

    join us

--- components/Blocks/JoinUs/index.vue
+++ components/Blocks/JoinUs/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="join-us">
+    <h2 class="join-us__title">{{ block.primary.title }}</h2>
+    <CTA class="join-us__cta" :url="block.primary.cta_link" :text="block.primary.cta_text" :is-nuxt-link="true"/>
   </div>
 </template>
 <script lang="ts" setup>
@@ -12,10 +14,22 @@ const props = defineProps<{
 
 </script>
 <style scoped lang="sass">
-.sponsors
+.join-us
   position: relative
   width: 100%
-  height: 100vh
-  height: calc(var(--vh, 1vh) * 100)
+  padding: 10rem 0
+
+  &__title
+    @include h1()
+    width: 100%
+    text-align: center
+    color: $white
+    margin-bottom: 10rem
+
+  &__cta
+    position: relative
+    left: 50%
+    transform: translateX(-50%)
+    margin: 8rem 0
 
 </style>
