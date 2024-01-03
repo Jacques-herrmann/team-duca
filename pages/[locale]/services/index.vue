8db7c27938262744d925ceda5960fa9729287b43
Jacques <jacques@laphase5.com>
2024-01-03 18:01:21 +0100

    animation contact && service

--- pages/[locale]/services/index.vue
+++ pages/[locale]/services/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="service-page">
+  <div class="service-page" ref="root">
     <figure-element class="service-page__cover" :image="servicePage?.data.cover"></figure-element>
-    <h1 class="service-page__title">{{servicePage?.data.title}}</h1>
+    <h1 class="service-page__title">
+      <div v-for="t in title"><span>{{t}}</span></div>
+    </h1>
     <div class="service-page__right">
       <prismic-rich-text class="service-page__content" :field="servicePage?.data.content"></prismic-rich-text>
       <CTA class="service-page__right__cta" :text="servicePage?.data.cta_text" :url="servicePage?.data.cta_link" :is-nuxt-link="true"/>
@@ -9,9 +11,11 @@
   </div>
 </template>
 <script lang="ts" setup>
+import A from '@/assets/animations'
+import gsap from 'gsap'
 
 const prismic = usePrismic();
-const route = useRoute();
+const store = useIndexStore();
 const page = usePage();
 
 const {data: servicePage } = useAsyncData("[services]", () => prismic.client.getSingle('services'))
@@ -26,6 +30,27 @@ useHead({
     },
   ],
 });
+
+const root = ref<HTMLElement | null>(null)
+const title = computed(() => servicePage.value?.data.title.split('\n'))
+const tl = gsap.timeline({paused: true})
+
+watch(() => store.isTransitionVisible, (value) => {
+  if(!value) {
+    setTimeout(() => {
+      tl.play()
+    }, 280)
+  }
+})
+
+
+onMounted(() => {
+  tl.from(root.value?.querySelectorAll(".service-page__cover") as NodeList, A.imageWidth, 0)
+  tl.from(root.value?.querySelectorAll(".service-page__title span") as NodeList, A.h2, 0.4)
+  tl.from(root.value?.querySelectorAll(".service-page__content") as NodeList, A.opacity, 0.6)
+  tl.from(root.value?.querySelectorAll(".service-page__cta") as NodeList, A.opacity, 0.8)
+
+})
 </script>
 
 <style scoped lang="sass">
@@ -39,24 +64,28 @@ useHead({
 
   &__title
     @include h1()
+    height: 6.1rem
     position: absolute
-    top: 160px
+    top: 100px
     left: 45%
     font-weight: 800
     color: $white
+    & > div
+      overflow: hidden
+    & span
+      display: inline-block
 
   &__right
     width: 45%
-    margin-top: 14rem
+    margin-top: 18rem
     padding: 0 45px
     display: flex
     flex-direction: column
     justify-content: flex-end
     @include text()
     &__cta
-      margin-top: 100px
+      margin-top: 60px
       align-self: end
-
 
   &__cover
     width: 55%
